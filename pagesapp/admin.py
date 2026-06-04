79cf7f3bba769128e538498b502104c55e40ee85
asilbek <asil@gmail.com>
2026-06-04 09:32:22 +0500

    birinchi komment

--- pagesapp/admin.py
+++ pagesapp/admin.py
@@ -1,3 +1,72 @@
-from django.contrib import admin
+from django.contrib import admin,messages
+from .models import Muallif, Janr,Kitob,KitobNusxa,Ijara
 
-# Register your models here.
+admin.site.register(Muallif)
+admin.site.register(Janr)
+
+
+
+
+
+
+
+
+
+
+class KitobNusxaInline(admin.TabularInline):
+    model = KitobNusxa
+    extra = 1  
+
+
+
+@admin.action(description="Tanlanganlarni mavjud deb belgilash")
+def make_available(modeladmin, request, queryset):
+    updated = queryset.update(mavjud=True)
+    modeladmin.message_user(
+        request,
+        f"{updated} ta kitob muvaffaqiyatli 'Mavjud' deb belgilandi.",
+        messages.SUCCESS,
+    )
+
+
+@admin.action(description="Narxni 10% tushirish")
+def discount_price(modeladmin, request, queryset):
+    count = 0
+    for kitob in queryset:
+
+        kitob.narx = kitob.narx * 0.9
+        kitob.save()
+        count += 1
+    modeladmin.message_user(
+        request,
+        f"{count} ta kitobning narxi 10% ga arzonlashtirildi.",
+        messages.SUCCESS,
+    )
+
+
+
+@admin.register(Kitob)
+class KitobAdmin(admin.ModelAdmin):
+    list_display  = ('nomi', 'muallif', 'nashr_yili', 'narx', 'mavjud')
+    list_filter   = ('mavjud', 'til', 'janrlar', 'nashr_yili')
+    search_fields = ('nomi', 'muallif__ism')   
+    ordering   = ('-nashr_yili',)   
+
+readonly_fields = ('qoshilgan_sana',)
+
+fieldsets = (
+    ('Asosiy ma\'lumot', {
+    'fields': ('nomi', 'muallif', 'janrlar'),
+    }),
+    ('Qo\'shimcha', {
+    'fields': ('til', 'nashr_yili', 'narx', 'mavjud', 'qoshilgan_sana'),
+    'classes': ('collapse',),
+    }),
+)
+list_per_page= 20
+
+
+inlines = [KitobNusxaInline] 
+actions = [make_available,discount_price]
+
+
--- pagesapp/models.py
+++ pagesapp/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+class Muallif(models.Model):
+    ism=models.CharField(max_length=130)
+    tugilgan_yili=models.IntegerField()
+    davlat=models.CharField(max_length=130)
+
+
+    def __str__(self):
+        return self.ism
+
+
+class Janr(models.Model):
+    nomi=models.CharField(max_length=100,unique = True)
+
+
+    def __str__(self):
+        return self.nomi
+
+
+
+
+
+class Kitob(models.Model):
+    nomi = models.CharField(max_length=200)
+    muallif = models.ForeignKey(Muallif, on_delete=models.CASCADE)
+    janrlar = models.ManyToManyField(Janr)
+    til = models.CharField(max_length=2, default='uz')
+    nashr_yili = models.IntegerField()
+    narx = models.DecimalField(max_digits=10, decimal_places=2)
+    mavjud = models.BooleanField(default=True)
+    qoshilgan_sana = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nomi
+
+
+
+
+class KitobNusxa(models.Model):
+    kitob=models.ForeignKey(Kitob, on_delete=models.CASCADE)
+    integral_raqam = models.CharField(max_length=200)
+    holat=models.CharField(max_length=200)
+    
+
+
+class Ijara(models.Model):
+    nusxa=models.ForeignKey(KitobNusxa, on_delete=models.CASCADE)
+    oluvchi=models.CharField(max_length=200)
+    olingan_sana = models.DateField()
+    qaytarilgan=models.BooleanField(default=True)
+
+
+    
+
